93e5c3bdfff04806f210043e326f651046a5ac2e
Sérgio Seeler <seelerfernandes1@gmail.com>
2023-03-16 18:35:02 -0300

    menu ativo e parametros url

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//Ativar Links do menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+//Ativar itens do Orçamento
+
+const paramentro = new URLSearchParams(location.search);
+
+function ativarProduto(paramentro) {
+  const elemento = document.getElementById(paramentro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  console.log(elemento);
+}
+
+paramentro.forEach(ativarProduto);
