e928b99071bdb2ca7d9a81bf209a464b73547ce5
Sérgio Seeler <seelerfernandes1@gmail.com>
2023-03-17 19:56:44 -0300

    galeria bicicletas

--- js/script.js
+++ js/script.js
@@ -45,3 +45,23 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+//Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 937px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  console.log(img);
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
